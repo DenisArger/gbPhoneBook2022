9cd86431e90773ee7ef38ed1aab2a438ad335686
adm217 <adm217@kbsp.com>
2022-10-21 07:48:09 +0300

    add module show

---
+++ modules/show_note.py
@@ -0,0 +1,11 @@
+import csv
+
+
+def view_note():
+    counter = int(1)
+    with open('database.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            print(f"{counter}. ", row["Фамилия"],
+                  row["Имя"], row["Телефон"], row["Описание"])
+            counter += 1
